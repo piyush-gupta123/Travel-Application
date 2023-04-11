5a3b14a5001cf6c8561ce7fd445f7370b845407f
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2023-04-11 15:14:59 +0530

    Deployment in progress

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use("/posts", postRouter);
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URL).catch((err) => console.log(err));
 
-if(process.env.NODE_ENV ==="production"){
-  app.use(express.static('client/build'));
-  // const path = require("path")
-  // app.get('*',(req,res)=>{
-  //   res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  // })
-}
+// if(process.env.NODE_ENV ==="production"){
+//   app.use(express.static('client/build'));
+//   // const path = require("path")
+//   // app.get('*',(req,res)=>{
+//   //   res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//   // })
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT} successfully`);
