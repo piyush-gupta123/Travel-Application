b6abc49f754914b28310b0b700d0cbc94cfbd985
piyush-gupta123 <95485177+piyush-gupta123@users.noreply.github.com>
2023-04-11 14:16:15 +0530

    Hosting in Progress

--- server/index.js
+++ server/index.js
@@ -2,19 +2,28 @@ import express from "express";
 import mongoose from "mongoose";
 import postRouter from "./Routes/posts-routes";
 import userRouter from "./Routes/user-routes";
-import cors from 'cors';
+import cors from "cors";
+import dotenv from "dotenv";
 mongoose.set("strictQuery", true);
 
 const app = express();
+dotenv.config();
 app.use(cors());
 // app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-app.use('/user',userRouter);
-app.use('/posts',postRouter);
-mongoose
-  .connect(
-    `mongodb+srv://Piyush_Gupta:Piyush%401234@cluster0.gxwijuy.mongodb.net/Travel?retryWrites=true&w=majority`
-  )
-  .then(() => app.listen(5000))
-  .then(() => console.log(`Server is connected successfully at PORT 5000`))
-  .catch((err) => console.log(err));
+app.use("/user", userRouter);
+app.use("/posts", postRouter);
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URL).catch((err) => console.log(err));
+
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static('client/build'));
+  // const path = require("path")
+  // app.get('*',(req,res)=>{
+  //   res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  // })
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT} successfully`);
+});
